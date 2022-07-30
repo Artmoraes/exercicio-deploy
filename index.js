2ac52a8b7b0d250ff0c8400cd170af676b0121dd
Arthur Barros <artmorraes1@gmail.com>
2022-07-29 22:02:29 -0300

    Adiciona mensagem de response por variável de ambiente

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
+// const express = require('express');
+
+// const app = express();
+
+// app.get('/', (req, res) => {
+//   res.send('Está vivo!!!')
+// });
+
+// const port = process.env.PORT || 3000;
+
+// app.listen(port);
+// console.log(`Escutando na porta ${port}`);
+
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Está vivo!!!')
-});
-
 const port = process.env.PORT || 3000;
+const message = process.env.MESSAGE || 'Está vivo!!!';
+
+app.get('/', (req, res) => {
+  res.send(message);
+});
 
 app.listen(port);
 console.log(`Escutando na porta ${port}`);
